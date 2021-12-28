65ff9f8fa172ef5f6a2669fb120cffdc746d1681
Alexandre Bekor <contato@alexandrebekor.com>
2021-12-28 12:12:14 -0300

    added mongoose

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+// ejs
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+const mongodb = "mongodb://localhost:27017/poc-auth-passport"
+const mongoose = require('mongoose')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const routePages = require('./routes/pages')
+app.use('/', routePages)
+
+// assets
+app.use(express.static(path.join(__dirname, 'public')))
+
+const User = require('./models/users')
+mongoose
+    .connect(mongodb)
+    .then(() => {
+        User.create({
+            username: "Alexandre",
+            password: "12345",
+            roles: "Admin"
+        })
+        app.listen(port, console.log('Server is runnig'))
+    })
+    .catch(error => console.log(error))
